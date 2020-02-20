06bdd50202408656fcf399034dd8601f4e861661
Don Mallory <dmm333@gmail.com>
2020-02-20 16:40:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,11 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	{railroads: {pieces: 4, rent_in_dollars: {}}	}
+	{railroads: {pieces: 4, rent_in_dollars: {
+														one: 1, two: 2, three: 3, four: 4
+		}
+		}
+		}
 
 end
 
